867dbc897a78c276e240ed50a4c88db8f5c934a7
Michael Jordan <michael@glowingpixel.com>
2025-08-11 17:00:04 -0700

    Initial

---
+++ qtlogrelay/__init__.py
@@ -0,0 +1,1 @@
+from ._qtlogrelay import QtLogRelayHandler
---
+++ qtlogrelay/_qtlogrelay.py
@@ -0,0 +1,32 @@
+import logging
+from PySide6 import QtCore
+
+class QtLogRelayHandler(logging.Handler):
+	"""
+	A python `logging` handler to emit a Qt signal when a new event is logged.
+	"""
+
+	class _RelaySignals(QtCore.QObject):
+		"""QtObject for relay signals"""
+
+		logEventReceived = QtCore.Signal(logging.LogRecord)
+		"""A new log record has been received"""
+
+		def __init__(self, parent=None):
+			super().__init__(parent=parent)
+
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		self._signals = self._RelaySignals(parent=self)
+		self.logEventReceived = self._signals.logEventReceived
+		"""A new log record has been received"""
+	
+	def emit(self, record:logging.LogRecord):
+		"""Do a log real nice"""
+
+		self.logEventReceived.emit(record)
+
+		# NOTE: Investigate queued connection when connecting to slot:
+		# self.logEventReceived.connect(slot, QtCore.Qt.QueuedConnection)
